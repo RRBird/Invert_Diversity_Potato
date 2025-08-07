664c35b8ae6f583c10cfe6499c7c466def1adbd5
Rhiannon R Bird <rhiannon.bird@uq.edu.au>
2025-08-07 11:24:17 +1000

    Loop for step 2 is up and running but need to figure out if rescaleing is needed

--- 02. Scripts/Main Script.R
+++ 02. Scripts/Main Script.R
@@ -27,6 +27,8 @@ Richlist1 <- list()
 allsum <- list()
 
 for (i in rich_names) {
+  allsum <- list()
+  
 #run models and collect summaries
     null <- glmmTMB(as.formula(paste(i, "~ 1 + (1 | Field)")), family = nbinom2, data = ModelRich2)
     allsum [[1]] <- summary(null)
@@ -81,22 +83,105 @@ Richlist1[[1]]
 
 ##Step 2 - Environmental Variables----
 
-#can we do a similar thing but would need to be able to shift the variables already there for each 
-#Will probably need to store the formula for each group that needs to be included in a list or something -> Group = Position * Age (or something)
-#Possibly a for loop within a for loop???
+rich_names
+Rich_design <- list(
+  All = "Day_Sampled * Crop_Age_Days",
+  Predator = "Day_Sampled * Crop_Age_Days",
+  Herbivore = "Position + Day_Sampled",   
+  Omnivore = "Position + Crop_Age_Days * Day_Sampled",
+  Fungivore = "Position + Crop_Age_Days * Day_Sampled",     
+  Hematophagous = "Position + Crop_Age_Days * Day_Sampled",
+  Web = "Position * Day_Sampled + Crop_Age_Days",          
+  Active_Hunting = "Day_Sampled",
+  Ambush_Hunting = "Position + Crop_Age_Days * Day_Sampled",
+  Hawking = "Position + Crop_Age_Days * Day_Sampled",      
+  Size_1 = "Day_Sampled + Crop_Age_Days",
+  Size_2 = NULL,      
+  Size_3 = "Day_Sampled",
+  Size_4 = "Position + Crop_Age_Days * Day_Sampled",
+  Always_Winged = "Position + Crop_Age_Days * Day_Sampled",
+  Develops_Wings = "Crop_Age_Days",
+  Wingless = "Position * Crop_Age_Days + Day_Sampled",
+  Polymorphic = "Position * Crop_Age_Days * Day_Sampled")
+head(Rich_design)
 
-#Can i have t = 1 (add to t each time)
-#then inside model richformulas[[1]]
-#e.g. glmmTMB(as.formula(paste(i, "~ richformulas[[1]] + Envrio + (1 | Field)")), family = nbinom2, data = ModelRich2)
+Richlist2 <- list()
 
-#small trial
-trial <- list()
-trial [[1]] <- "Day_Sampled * Crop_Age_Days"
-glmmTMB(All ~ trial [[1]] + GC + (1 | Field), family = nbinom2, data = ModelRich2)
-#that didn't work so see if you can fiund a way around it
 
-#Don't forget need to run the model from step 1 and all the environmental models 
+for (i in rich_names) {
+  allsum <- list()
+  
+  design_formula_part <- Rich_design[[i]]
+  
+  cat("=== Processing group:", i, "===\n") #track where loop is up to
+  cat("Design formula part:", design_formula_part, "\n") #making sure it's taken the right formula
+  
+  #Handle groups without null model as top in step 1
+  if (is.null(design_formula_part) || design_formula_part == "") {
+    # No design variables - intercept only model
+    base_formula <- paste(i, "~ 1 + (1 | Field)")} else {
+    # Has design variables
+    base_formula <- paste(i, "~", design_formula_part, "+ (1 | Field)")}
+  base_model <- glmmTMB(as.formula(base_formula),family = nbinom2, data = ModelRich2)
+  allsum[[1]] <- summary(base_model)
+  
+  if (is.null(design_formula_part) || design_formula_part == "") {
+    height_formula <- paste(i, "~ Height + (1 | Field)")} else {
+      height_formula <- paste(i, "~", design_formula_part, "+ Height + (1 | Field)")}
+  height_model <- glmmTMB(as.formula(height_formula), family = nbinom2, data = ModelRich2)
+  allsum[[2]] <- summary(height_model)
+  
+  if (is.null(design_formula_part) || design_formula_part == "") {
+    gc_formula <- paste(i, "~ GC  + (1 | Field)")} else {
+      gc_formula <- paste(i, "~", design_formula_part, "+ GC + (1 | Field)")}
+  gc_model <- glmmTMB(as.formula(gc_formula), family = nbinom2, data = ModelRich2)
+  allsum[[3]] <- summary(gc_model)
+  
+  if (is.null(design_formula_part) || design_formula_part == "") {
+    Fsize_formula <- paste(i, "~ Field_Area_m2  + (1 | Field)")} else {
+      Fsize_formula <- paste(i, "~", design_formula_part, "+ Field_Area_m2 + (1 | Field)")}
+  Fsize_model <- glmmTMB(as.formula(Fsize_formula), family = nbinom2, data = ModelRich2)
+  allsum[[4]] <- summary(Fsize_model)
+  
+  if (is.null(design_formula_part) || design_formula_part == "") {
+    water_formula <- paste(i, "~ X1km_Prop_Water  + (1 | Field)")} else {water_formula <- paste(i, "~", design_formula_part, "+ X1km_Prop_Water + (1 | Field)")}
+  water_model <- glmmTMB(as.formula(water_formula), family = nbinom2, data = ModelRich2)
+  allsum[[5]] <- summary(water_model)
+  
+  if (is.null(design_formula_part) || design_formula_part == "") {
+    NDVIf_formula <- paste(i, "~ NDVImean_Field  + (1 | Field)")} else {NDVIf_formula <- paste(i, "~", design_formula_part, "+ NDVImean_Field + (1 | Field)")}
+  NDVIf_model <- glmmTMB(as.formula(NDVIf_formula), family = nbinom2, data = ModelRich2)
+  allsum[[6]] <- summary(NDVIf_model)
+  
+  if (is.null(design_formula_part) || design_formula_part == "") {
+    NDVI1km_formula <- paste(i, "~ NDVIsum_1km  + (1 | Field)")} else {
+      NDVI1km_formula <- paste(i, "~", design_formula_part, "+ NDVIsum_1km + (1 | Field)")}
+  NDVI1km_model <- glmmTMB(as.formula(NDVI1km_formula), family = nbinom2, data = ModelRich2)
+  allsum[[7]] <- summary(NDVI1km_model)
+  
+  
+  #collect models
+  tempmodlist <- list("base" = base_model, "height" = height_model, 
+                      "GC" = gc_model, "Feild Size" = Fsize_model,
+                      "water" = water_model,"Field NDVI" = NDVIf_model,
+                      "NDVI 1km" = NDVI1km_model)
+  
+  #check which models didn't coverge/had issue to exclude from AIC
+  has_na <- as.logical(sapply(allsum, function(model) is.na(model$AICtab[1])))
+  na_models <- allsum[has_na]
+  cleaned_models <- tempmodlist[!has_na] #remove models with NA as AIC
+  
+  Richlist2 [[i]] <- aictab(cleaned_models,modnames = NULL)
+  
+}
 
+length(Richlist2)
+
+Richlist2[[1]]
+
+
+#Loop is working currently but might need to rescale variables to make sure that models will converge??? 
+#need to work out what my system is for this?????
 
 ##Step 3 - Check for spatial autocorrelation----
 
