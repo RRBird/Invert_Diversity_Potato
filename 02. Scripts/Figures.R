c5c099f4219b3bf20fb89634f33f9bff1c87eef3
Rhiannon R Bird <rhiannon.bird@uq.edu.au>
2025-08-18 16:44:39 +1000

    More doing richness figs

--- 02. Scripts/Figures.R
+++ 02. Scripts/Figures.R
@@ -54,9 +54,9 @@ par(mar=c(4,4,2,2),mfrow=c(2,2),mgp=c(2.5,1,0),xpd = T)
 
 plot(x = ModelRich2$Day_Scaled,y = ModelRich2$Predator,xlab = expression("Day Sampled"),ylab = 'Predator Species Richness', type = 'p', pch = 16,cex =0.2,col = 'black', las = 1, lwd = 2,xaxt ="n")
 mtext(side=3,line=0,at = -2.1,'a)',cex=1)
-mtext(side=1,line=3,at = -1.5,'Autumn/Winter',cex=0.8)
+mtext(side=1,line=3,at = -1.5,'Winter',cex=0.8)
 mtext(side=1,line=3,at = 1.5,'Spring',cex=0.8)
-arrows(-0.7,-3.7,1.15,-3.7, length =0.1)
+arrows(-1.1,-3.7,1.15,-3.7, length =0.1)
 
 axis(side=1, at=seq(from=min(richpred_preds.1$Day_Scaled),to=max(richpred_preds.1$Day_Scaled),length.out=6),labels=round(seq(from=min(ModelRich2$Day_Sampled),to=max(ModelRich2$Day_Sampled),length.out=6),-1),cex.axis = 0.9)
 
@@ -99,7 +99,6 @@ BB <- richpred_herb.1$Position == Predictions_Position[1]
 dev.new(height=5,width=10,dpi=80,pointsize=14,noRStudioGD = T)
 par(mar=c(4,4,2,2),mfrow=c(1,2),mgp=c(2.5,1,0),xpd = T)
 
-#TO DO - Am I keeping the raw data on there----
 #position
 #To make sure the order is right for position had to do 2:1
 
@@ -115,9 +114,9 @@ points(x = jitter(raw_x, factor = 1),y = ModelRich2$Herbivore, pch = 16, cex = 0
 
 plot(x = ModelRich2$Day_Scaled,y = ModelRich2$Herbivore,xlab = expression("Day Sampled"),ylab = 'Herbivore Species Richness', type = 'p', pch = 16,cex =0.2,col = 'black', las = 1, lwd = 2,xaxt ="n")
 mtext(side=3,line=0,at = -2.1,'b)',cex=1)
-mtext(side=1,line=3,at = -1.5,'Autumn/Winter',cex=0.8)
+mtext(side=1,line=3,at = -1.5,'Winter',cex=0.8)
 mtext(side=1,line=3,at = 1.5,'Spring',cex=0.8)
-arrows(-1,-1.2,1.25,-1.2, length =0.1)
+arrows(-1.2,-1.2,1.25,-1.2, length =0.1)
 
 axis(side=1, at=seq(from=min(richpred_herb.1$Day_Scaled),to=max(richpred_herb.1$Day_Scaled),length.out=6),labels=round(seq(from=min(ModelRich2$Day_Sampled),to=max(ModelRich2$Day_Sampled),length.out=6),-1))
 
@@ -152,8 +151,8 @@ head(richpred_web.1);dim(richpred_web.1)
 
 DD <- richpred_web.1$Position == "Inner" & richpred_web.1$Age_Scaled == Predictions_Age[10] & richpred_web.1$GC == Predictions_GC[10]
 DDD <- richpred_web.1$Position == "Outer" & richpred_web.1$Age_Scaled == Predictions_Age[10] & richpred_web.1$GC == Predictions_GC[10]
-DDDD <- richpred_web.1$Position == "Inner" & richpred_web.1$Day_Scaled == Predictions_Day[10] & richpred_web.1$GC == Predictions_GC[10]
-DDDDD <- richpred_web.1$Position == "Inner" & richpred_web.1$Day_Scaled == Predictions_Day[10] & richpred_web.1$Age_Scaled == Predictions_Age[10]
+DDDD <- richpred_web.1$Position == "Outer" & richpred_web.1$Day_Scaled == Predictions_Day[10] & richpred_web.1$GC == Predictions_GC[10]
+DDDDD <- richpred_web.1$Position == "Outer" & richpred_web.1$Day_Scaled == Predictions_Day[10] & richpred_web.1$Age_Scaled == Predictions_Age[10]
 
 
 dev.new(height=10,width=10,dpi=80,pointsize=14,noRStudioGD = T)
@@ -162,9 +161,9 @@ par(mar=c(4,4,2,2),mfrow=c(2,2),mgp=c(2.5,1,0),xpd = T)
 #Position * Day 
 
 plot(x = ModelRich2$Day_Scaled,y = ModelRich2$Web, xlab = "Day Sampled",ylab = 'Web Building Species Richness',type = 'p', pch = 16,cex =0.2,col = 'black', las = 1, lwd = 2, xaxt = 'n')
-mtext(side=1,line=3,at = -1.5,'Autumn/Winter',cex=0.8)
+mtext(side=1,line=3,at = -1.5,'Winter',cex=0.8)
 mtext(side=1,line=3,at = 1.5,'Spring',cex=0.8)
-arrows(-0.7,-2.1,1.1,-2.1, length =0.1)
+arrows(-1.1,-2.1,1.1,-2.1, length =0.1)
 mtext(side=3,line=0,at = -2,'a)',cex=1)
 
 axis(side=1, at=seq(from=min(richpred_web.1$Day_Scaled),to=max(richpred_web.1$Day_Scaled),length.out=6),labels=round(seq(from=min(ModelRich2$Day_Sampled),to=max(ModelRich2$Day_Sampled),length.out=6),-1),cex.axis = 0.9)
@@ -198,44 +197,32 @@ lines(x=richpred_web.1$GC[DDDDD],y = richpred_web.1$fit[DDDDD],lwd = 2,col = 'gr
 ###Active Hunting----
 summary(Rich_Active) # Day * Age
 
-#NEED TO COME BACK TO THIS----
 richpred_active.1 <- richpredresults[[6]]
 head(richpred_active.1);dim(richpred_active.1)
 
-#Trial and error to get these two - if you use min and max the fit predicted value is fine but the uci is at 200 at the max 
-#Can I pick these as the two representative things for age and talk about the high uci in the results or discussion?
-#Can I pick things that represent the relationship best or do I need to follow a protocol i.e. max and min, one standard deviation from mean etc?
+#working out days to represent winter and spring
+Predictions_Day
+seq(min(ModelRich2$Day_Sampled),max(ModelRich2$Day_Sampled),length.out=20) 
+Predictions_Day[4] #winter
+Predictions_Day[15] #Spring
 
-E <- richpred_active.1$Age_Scaled == Predictions_Age[12]
-EE <- richpred_active.1$Age_Scaled == Predictions_Age[20]
-
-E <- richpred_active.1$Age_Scaled == min(Predictions_Age)
-EE <- richpred_active.1$Age_Scaled == max(Predictions_Age)
-
-seq(min(ModelRich2$Crop_Age_Days),max(ModelRich2$Crop_Age_Days),length.out=20)[12]
-seq(min(ModelRich2$Crop_Age_Days),max(ModelRich2$Crop_Age_Days),length.out=20)[20]
+E <- richpred_active.1$Day_Scaled == Predictions_Day[4]
+EE <- richpred_active.1$Day_Scaled == Predictions_Day[15]
 
 
 dev.new(height=5,width=5,dpi=80,pointsize=14,noRStudioGD = T)
 par(mar=c(4,4,2,2),mgp=c(2.5,1,0),xpd = T)
 
-plot(x = ModelRich2$Day_Scaled,y = ModelRich2$Active_Hunting, xlab = "Day Sampled",ylab = 'Active Hunting Species Richness',type = 'p', pch = 16,cex =0.2,col = 'black', las = 1, lwd = 2, xaxt = 'n',ylim = c(0,15))
-mtext(side=1,line=3,at = -1.5,'Autumn/Winter',cex=0.8)
-mtext(side=1,line=3,at = 1.5,'Spring',cex=0.8)
-arrows(-1,-4.5,1.3,-4.5, length =0.1)
+plot(x = ModelRich2$Age_Scaled,y = ModelRich2$Active_Hunting, xlab = "Crop Age (Days)",ylab = 'Active Hunting Species Richness',type = 'p', pch = 16,cex =0.2,col = 'black', las = 1, lwd = 2, xaxt = 'n',ylim=c(0,7))
+axis(side=1, at=seq(from=min(richpred_active.1$Age_Scaled),to=max(richpred_active.1$Age_Scaled),length.out=6),labels=round(seq(from=min(ModelRich2$Crop_Age_Days),to=max(ModelRich2$Crop_Age_Days),length.out=6),-1),cex.axis = 0.9)
 
-axis(side=1, at=seq(from=min(richpred_active.1$Day_Scaled),to=max(richpred_active.1$Day_Scaled),length.out=6),labels=round(seq(from=min(ModelRich2$Day_Sampled),to=max(ModelRich2$Day_Sampled),length.out=6),-1),cex.axis = 0.9)
+polygon(x = c(richpred_active.1$Age_Scaled[E],rev(richpred_active.1$Age_Scaled[E])), y = c(richpred_active.1$lci[E],rev(richpred_active.1$uci[E])),col = rgb(0.7, 0.7, 0.7, 0.7),border = NA)
+lines(x=richpred_active.1$Age_Scaled[E],y = richpred_active.1$fit[E],lwd = 2,lty = 1, col = 'grey30')
 
-polygon(x = c(richpred_active.1$Day_Scaled[E],rev(richpred_active.1$Day_Scaled[E])), y = c(richpred_active.1$lci[E],rev(richpred_active.1$uci[E])),col = rgb(0.7, 0.7, 0.7, 0.7),border = NA)
-lines(x=richpred_active.1$Day_Scaled[E],y = richpred_active.1$fit[E],lwd = 2,lty = 1, col = 'grey30')
+polygon(x = c(richpred_active.1$Age_Scaled[EE],rev(richpred_active.1$Age_Scaled[EE])), y = c(richpred_active.1$lci[EE],rev(richpred_active.1$uci[EE])),col = rgb(0.7, 0.7, 0.7, 0.7),border = NA)
+lines(x=richpred_active.1$Age_Scaled[EE],y = richpred_active.1$fit[EE],lwd = 2,lty = 2, col = 'grey30')
 
-polygon(x = c(richpred_active.1$Day_Scaled[EE],rev(richpred_active.1$Day_Scaled[EE])), y = c(richpred_active.1$lci[EE],rev(richpred_active.1$uci[EE])),col = rgb(0.7, 0.7, 0.7, 0.7),border = NA)
-lines(x=richpred_active.1$Day_Scaled[EE],y = richpred_active.1$fit[EE],lwd = 2,lty = 2, col = 'grey30')
-
-legend('topleft',legend = c('Crop Age 84 Days', "Crop Age 109 Days"), lty = c(1,2), col = 'grey30',pt.cex = 1)
-
-
-
+legend('topright',legend = c('Winter', "Spring"), lty = c(1,2), col = 'grey30',pt.cex = 1)
 
 
 ###Size 1 (0-2.5)----
@@ -253,7 +240,6 @@ raw_x2 <- ifelse(ModelRich2$Position == "Inner", 1,
 
 dev.new(height=10,width=10,dpi=80,pointsize=14,noRStudioGD = T)
 par(mar=c(4,4,2,2),mfrow=c(2,2),mgp=c(2.5,1,0),xpd = T)
-#TO DO - Am I keeping the raw data on there?----
 
 #position
 
@@ -261,7 +247,7 @@ plot(x = 1:2,y = richpred_size1.1$fit [FF],xlab = " ",ylab = 'Size 0-2.5cm Speci
 mtext(side=3,line=0,at = -0.7,'a)',cex=1)
 axis(side=1,at=1:2,labels=c('Inner','Outer'))
 
-arrows(x0=1:2, y0=richpred_size1.1$lci [FF],x1=1:2, y1=richpred_size1.1$uci[FF],angle=90,length=0.2, code=3, lwd=2,col = "black")
+arrows(x0=1:2, y0=richpred_size1.1$lci [FF],x1=1:2, y1=richpred_size1.1$uci[FF],angle=90,length=0.1, code=3, lwd=2,col = "black")
 
 points(x = jitter(raw_x2, factor = 1),y = ModelRich2$Size_1, pch = 16, cex = 0.4, col = "grey")
 
@@ -280,9 +266,9 @@ lines(x=richpred_size1.1$Age_Scaled[FFF],y = richpred_size1.1$fit[FFF],lwd = 2,c
 
 plot(x = ModelRich2$Day_Scaled,y = ModelRich2$Size_1,xlab = expression("Day Sampled"),ylab = 'Size 0-2.5cm Species Richness', type = 'p', pch = 16,cex =0.2,col = 'black', las = 1, lwd = 2,xaxt ="n")
 mtext(side=3,line=0,at = -2.1,'a)',cex=1)
-mtext(side=1,line=3,at = -1.5,'Autumn/Winter',cex=0.8)
+mtext(side=1,line=3,at = -1.5,'Winter',cex=0.8)
 mtext(side=1,line=3,at = 1.5,'Spring',cex=0.8)
-arrows(-0.7,-2.1,1.15,-2.1, length =0.1)
+arrows(-1.1,-2.1,1.15,-2.1, length =0.1)
 
 axis(side=1, at=seq(from=min(richpred_size1.1$Day_Scaled),to=max(richpred_size1.1$Day_Scaled),length.out=6),labels=round(seq(from=min(ModelRich2$Day_Sampled),to=max(ModelRich2$Day_Sampled),length.out=6),-1),cex.axis = 0.9)
 
@@ -307,14 +293,61 @@ lines(x=richpred_size2.1$Age_Scaled,y = richpred_size2.1$fit,lwd = 2,col = 'grey
 
 axis(side=1, at=seq(from=min(richpred_size2.1$Age_Scaled),to=max(richpred_size2.1$Age_Scaled),length.out=6),labels=round(seq(from=min(ModelRich2$Crop_Age_Days),to=max(ModelRich2$Crop_Age_Days),length.out=6),-1))
 
-###Size 3 (5-10) TO DO----
+###Size 3 (5-10)----
 summary(Rich_Size3) #Position + Age * Day + Water
 
 richpred_size3.1 <- richpredresults[[9]]
 head(richpred_size3.1);dim(richpred_size3.1)
 
-G
-GG
+#working out days to represent winter and spring
+Predictions_Day
+seq(min(ModelRich2$Day_Sampled),max(ModelRich2$Day_Sampled),length.out=20) 
+Predictions_Day[4] #winter
+Predictions_Day[15] #Spring
+
+G <- richpred_size3.1$Age_Scaled == Predictions_Age[10] & richpred_size3.1$Day_Scaled == Predictions_Day[10] & richpred_size3.1$X1km_Prop_Water == Predictions_Water[10]
+GG <- richpred_size3.1$Day_Scaled == Predictions_Day[4] & richpred_size3.1$X1km_Prop_Water == Predictions_Water[10] & richpred_size3.1$Position == "Outer"
+GGG <- richpred_size3.1$Day_Scaled == Predictions_Day[15] & richpred_size3.1$X1km_Prop_Water == Predictions_Water[10] & richpred_size3.1$Position == "Outer"
+GGGG <- richpred_size3.1$Day_Scaled == Predictions_Day[10] & richpred_size3.1$Position == "Outer" & richpred_size3.1$Age_Scaled == Predictions_Age[10] 
+
+raw_x3 <- ifelse(ModelRich2$Position == "Inner", 1, 
+                 ifelse(ModelRich2$Position == "Outer", 2, NA))
+
+
+dev.new(height=10,width=10,dpi=80,pointsize=14,noRStudioGD = T)
+par(mar=c(4,4,2,2),mfrow=c(2,2),mgp=c(2.5,1,0),xpd = T)
+
+#position
+
+plot(x = 2:1,y = richpred_size3.1$fit [G],xlab = " ",ylab = 'Size 5-10cm Species Richness', type = 'p',pch = 16,cex =2.5,col = 'black', las = 1,xaxt = "n",ylim = c(0,1), xlim = c(0,3))
+mtext(side=3,line=0,at = -0.7,'a)',cex=1)
+axis(side=1,at=2:1,labels=c('Outer','Inner'))
+
+arrows(x0=2:1, y0=richpred_size3.1$lci[G],x1=2:1, y1=richpred_size3.1$uci[G],angle=90,length=0.2, code=3, lwd=2,col = "black")
+
+points(x = jitter(raw_x2, factor = 1),y = ModelRich2$Size_3, pch = 16, cex = 0.4, col = "grey")
+
+#Age * Day
+
+plot(x = ModelRich2$Age_Scaled,y = ModelRich2$Size_3,xlab = "Crop Age (Days)",ylab = '5-10cm Species Richness', type = 'p', pch = 16,cex =0.2,col = 'black', las = 1, lwd = 2, xaxt = 'n',ylim = c(0,7))
+mtext(side=3,line=0,at = -2.6,'b)',cex=1)
+axis(side=1, at=seq(from=min(richpred_size3.1$Age_Scaled),to=max(richpred_size3.1$Age_Scaled),length.out=6),labels=round(seq(from=min(ModelRich2$Crop_Age_Days),to=max(ModelRich2$Crop_Age_Days),length.out=6),-1),cex.axis = 0.9)
+
+polygon(x = c(richpred_size3.1$Age_Scaled[GG],rev(richpred_size3.1$Age_Scaled[GG])), y = c(richpred_size3.1$lci[GG],rev(richpred_size3.1$uci[GG])),col = rgb(0.5, 0.5, 0.5, 0.5),border = NA)
+lines(x=richpred_size3.1$Age_Scaled[GG],y = richpred_size3.1$fit[GG],lwd = 2,col = 'grey30',lty = 1)
+
+polygon(x = c(richpred_size3.1$Age_Scaled[GGG],rev(richpred_size3.1$Age_Scaled[GGG])), y = c(richpred_size3.1$lci[GGG],rev(richpred_size3.1$uci[GGG])),col = rgb(0.5, 0.5, 0.5, 0.5),border = NA)
+lines(x=richpred_size3.1$Age_Scaled[GGG],y = richpred_size3.1$fit[GGG],lwd = 2,col = 'grey30',lty = 2)
+
+legend('topleft',legend = c('Winter', "Spring"), lty = c(1,2), col = 'grey30',pt.cex = 1)
+
+#Water
+
+plot(x = ModelRich2$X1km_Prop_Water,y = ModelRich2$Size_3,xlab = expression("Proportion Water within 1km"),ylab = '5-10cm Species Richness', type = 'p', pch = 16,cex =0.2,col = 'black', las = 1, lwd = 2)
+mtext(side=3,line=0,at = -0.5,'c)',cex=1)
+
+polygon(x = c(richpred_size3.1$X1km_Prop_Water[GGGG],rev(richpred_size3.1$X1km_Prop_Water[GGGG])), y = c(richpred_size3.1$lci[GGGG],rev(richpred_size3.1$uci[GGGG])),col = rgb(0.5, 0.5, 0.5, 0.5),border=NA)
+lines(x=richpred_size3.1$X1km_Prop_Water[GGGG],y = richpred_size3.1$fit[GGGG],lwd = 2,col = 'grey30')
 
 
 ###Develops Wings----
@@ -359,7 +392,7 @@ head(richpred_Wless.1);dim(richpred_Wless.1)
 I <- richpred_Wless.1$Position == "Inner" & richpred_Wless.1$Day_Scaled == Predictions_Day[10]
 II <- richpred_Wless.1$Position == "Outer" & richpred_Wless.1$Day_Scaled == Predictions_Day[10]
 
-III <- richpred_Wless.1$Position == "Inner" & richpred_Wless.1$Age_Scaled == Predictions_Age[10]
+III <- richpred_Wless.1$Position == "Outer" & richpred_Wless.1$Age_Scaled == Predictions_Age[10]
 
 dev.new(height=5,width=10,dpi=80,pointsize=14,noRStudioGD = T)
 par(mar=c(4,4,2,2),mfrow=c(1,2),mgp=c(2.5,1,0),xpd = T)
@@ -382,9 +415,9 @@ legend('topleft',legend = c('Inner', "Outer"), lty = c(1,2), col = 'grey30',pt.c
 #Day
 
 plot(x = ModelRich2$Day_Scaled,y = ModelRich2$Wingless, xlab = "Day Sampled",ylab = 'Wingless Species Richness',type = 'p', pch = 16,cex =0.2,col = 'black', las = 1, lwd = 2, xaxt = 'n')
-mtext(side=1,line=3,at = -1.5,'Autumn/Winter',cex=0.8)
+mtext(side=1,line=3,at = -1.5,'Winter',cex=0.8)
 mtext(side=1,line=3,at = 1.5,'Spring',cex=0.8)
-arrows(-1,-1.8,1.3,-1.8, length =0.1)
+arrows(-1.2,-1.8,1.2,-1.8, length =0.1)
 
 axis(side=1, at=seq(from=min(richpred_Wless.1$Day_Scaled),to=max(richpred_Wless.1$Day_Scaled),length.out=6),labels=round(seq(from=min(ModelRich2$Day_Sampled),to=max(ModelRich2$Day_Sampled),length.out=6),-1),cex.axis = 0.9)
 
