e3de4d54f4f527724e6f5ed20e20db4f16088a89
Rhiannon R Bird <rhiannon.bird@uq.edu.au>
2025-11-13 10:57:13 +1000

    Need a break from looking at this now

--- 02. Scripts/RLQ Script.R
+++ 02. Scripts/RLQ Script.R
@@ -3,7 +3,18 @@
 library("ade4")
 library("dplyr")
 library("tidyverse")
-install.packages("vegan")
+library("vegan")
+
+Q_matrix$Size <- factor(Q_matrix$Size, 
+                        levels = c("0-2.5", "2.5-5", "5-10", 
+                                   ">10","No_Size", "Unknown"),
+                        ordered = FALSE) #to make it work properly
+str(Q_matrix)
+
+R_matrix$Position <- factor(R_matrix$Position, 
+                            levels = c("Inner", "Outer"),
+                            ordered = FALSE) #to make it work properly
+str(R_matrix)
 
 #Species responses to environmental gradients----
 
@@ -16,31 +27,62 @@ cca1 <- pcaiv(coa1, R_matrix, scannf = F)
 dev.new(height=10,width=10,dpi=80,pointsize=14,noRStudioGD = T)
 s.label(cca1$c1, clabel = 0)
 par(mar = c(0.1, 0.1, 0.1, 0.1))
-pointLabel(cca1$c1,row.names(cca1$c1), cex=0.7)
 s.arrow(cca1$cor[-1,], add.plot=TRUE)
 
+#RLQ Analysis---- 
+
+pca.traits <- dudi.hillsmith(Q_matrix, row.w = coa1$cw, scannf = FALSE)
+pca.env <- dudi.hillsmith(R_matrix, row.w = coa1$lw, scannf = FALSE)
+
+rlq1 <- rlq(pca.env, coa1, pca.traits, scannf = FALSE)
+summary(rlq1)
+
+dev.new(height=10,width=15,dpi=80,pointsize=14,noRStudioGD = T)
+plot(rlq1)
+
+## Percentage of co-Inertia for each axis
+100*rlq1$eig/sum(rlq1$eig)
+
+#To interpret the results, correlations can be computed:
+## weighted correlations axes / env.
+t(pca.env$tab)%*%(diag(pca.env$lw))%*%as.matrix(rlq1$mR)
+
+## weighted correlations axes / traits.
+t(pca.traits$tab)%*%(diag(pca.traits$lw))%*%as.matrix(rlq1$mQ)
+
+## correlations traits / env.
+rlq1$tab
+
+#bplot
+dev.new(height=10,width=10,dpi=80,pointsize=14,noRStudioGD = T)
+s.arrow(rlq1$c1, xlim=c(-1,1), boxes = FALSE)
+s.label(rlq1$li, add.plot=T, clab=1.5)
+
+#Another plot
+dev.new(height=10,width=10,dpi=80,pointsize=14,noRStudioGD = T)
+s.label(rlq1$lQ, clabel = 0)
+par(mar = c(0.1, 0.1, 0.1, 0.1))
+pointLabel(rlq1$lQ,row.names(rlq1$lQ), cex=0.7) #pointLabel didn't work so need an alternative to add names to this one
+
+#Classifying scores to obtain functional groups
+hc2 <- hclust(dist(rlq1$lQ), method = "ward.D")
+dev.new(height=20,width=40,dpi=80,pointsize=14,noRStudioGD = T)
+plot(hc2)
+
+#Calinsky-Harabasz criteria to find best partition 
+ntest <- 6
+res <- rep(0,ntest - 1)
+for (i in 2:ntest){
+  fac <- cutree(hc2, k = i)
+  res[i-1] <- calinski(tab=rlq1$lQ, fac = fac)[1]
+}
+#calinski ISN"T WORKING SO NEED TO FIGURE THAT OUT----
 
 
-#correspondence analysis on matrix L
-coa1 <- dudi.coa(species, scannf = F)
-head(coa1)
-
-
-
-#Step 1: Correspondence Analysis on L table
-dudi_L <- dudi.coa(L_matrix, scannf = FALSE, nf = 2)
-
-#Step 2: Hill-Smith analysis on Q table (handles mixed trait types)
-dudi_Q <- dudi.hillsmith(Q_matrix, scannf = FALSE, nf = 2, row.w = dudi_L$cw)
-
-#Step 3: PCA on R table
-dudi_R <- dudi.pca(R_matrix, scannf = FALSE, nf = 2, row.w = dudi_L$lw)
-
-#Step 4: RLQ analysis
-rlq_result <- rlq(dudi_R, dudi_L, dudi_Q, scannf = FALSE, nf = 2)
-
-pca.traits <- dudi.pca(traits,row.w = coa1$cw,scannf=F)
-
+dev.new(height=5,width=5,dpi=80,pointsize=14,noRStudioGD = T)
+par(mfrow=c(1,2))
+plot(2:ntest, res, type='b', pch=20, xlab="Number of groups", ylab = "C-H index")
+plot(3:ntest, diff(res), type='b', pch=20, xlab="Number of groups", ylab = "Diff in C-H index")
 
 
 
